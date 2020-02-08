a78a3b30cb6c29e8de0f35ea25b4922ad2533bb6
NetForceTeam <59515825+NetForceTeam@users.noreply.github.com>
2020-02-08 11:46:02 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+provider "google" {
+  project = "apt-hold-267119"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
+
+
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "f1-micro"
@@ -10,8 +22,9 @@ resource "google_compute_instance" "vm_instance" {
 
   network_interface {
     # A default network is created for all GCP projects
-    network       = "default"
+    network = "${google_compute_network.vpc_network.self_link}"
     access_config {
     }
   }
 }
+
